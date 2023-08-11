eabfdef79dd7569dca7100eba9254a6e49595402
Bubson <bubseatbubs@gmail.com>
2023-08-11 11:29:36 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,15 @@
 
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-  }
+    return (
+      <footer class="footer bg-dark py-5">
+      <div class="container grid grid-3">
+          <div>
+              <h1>404.
+              </h1>
+              <p>Where do you think you're going?</p>
+          </div>
+          <img src="https://raw.githubusercontent.com/Bubseatbubs/nt-news/master/img/creepy.png" alt="" />
+      </div>
+      </footer>
+    );
+}
